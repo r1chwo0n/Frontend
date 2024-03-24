57694c97736d0f9a61312a8e56cc24417cd45347
r1chwo0n <110924108+r1chwo0n@users.noreply.github.com>
2024-03-24 12:14:02 +0700

    Update Play.tsx

--- src/components/Play.tsx
+++ src/components/Play.tsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import HexagonGrid from "./HexagonGrid.tsx";
+import useWebSocket from "../customHook/useWebSocket.ts";
+import { useAppSelector } from "../store/hooks.ts";
+import { selectUsername } from "../store/Slices/usernameSlice.ts";
+import {
+  messageType,
+  selectWebSocket,
+} from "../store/Slices/webSocketSlice.ts";
+import Constructionplan from "./ConstructionPlan.tsx";
+import Wait from "./Wait.tsx";
 export default function Play() {
-  const [plan, setConstructionPlan] = useState<string>("");
-
-  const handleSendPlan = () => {
-    // Implement your logic to send the construction plan here
-    console.log("Sending construction plan:", plan);
-  };
+  const { sendPlan } = useWebSocket();
+  const [typedPlan, setTypedPlan] = useState<string>("");
+  const username = useAppSelector(selectUsername);
+  const webSocketState = useAppSelector(selectWebSocket);
 
   return (
     <>
       <div className="upleft">
-        <div className="pinkRectangle">
-          <div className="textPlay">Player 1’s turn</div>
+        <div className="pinkRectangle1">
+          <div className="textPlay"> {username.username}’s turn</div>
+          <img
+            className="w-20 h-30 left-[115px] top-[-20px] absolute"
+            src={username.pic}
+          />
         </div>
       </div>
       <div className="bottomdown">
@@ -24,24 +35,35 @@ export default function Play() {
         <div className="budget"></div>
       </div>
       <div className="hexagon">
-        <HexagonGrid width={700} height={520} size={45} />
+        <HexagonGrid width={760} height={550} size={53} />
       </div>
+      {/* 
       <div className="recConstruct">
-        <button
-          className="sendButt bg-blue-500 text-white hover:text-yellow-200 hover:bg-purple-500 py-2 px-10 focus:outline-none focus:shadow-outline"
-          type="submit"
-          onClick={handleSendPlan}
+ 
+      </div> */}
+
+      <div className="recConstruct">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            sendPlan(typedPlan, username.username);
+            setTypedPlan("");
+          }}
         >
-          Send
-        </button>
-        <textarea
-          className="rounded-xl resize-none writeConstruct"
-          id="Constructor Plan"
-          placeholder=" Construction Plan"
-          value={plan}
-          onChange={(e) => setConstructionPlan(e.target.value)}
-          required
-        ></textarea>
+          <textarea
+            className="rounded-xl resize-none writeConstruct"
+            placeholder=" Construction Plan"
+            value={typedPlan}
+            onChange={(e) => setTypedPlan(e.target.value)}
+            required
+          ></textarea>
+          <button
+            className="sendButt bg-blue-500 text-white hover:text-yellow-200 hover:bg-purple-500 py-2 px-10 focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Send
+          </button>
+        </form>
       </div>
     </>
   );
